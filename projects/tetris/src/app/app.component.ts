0bf82aea312ae2b94e62e98c9bac6f7f5bc3af28
Richard Sithole <richardsit@gmail.com>
2021-01-04 11:37:26 +0100

    Add ElectronService to Angular

--- projects/tetris/src/app/app.component.ts
+++ projects/tetris/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { ElectronService } from "./core/services";
+import * as AppConfig from "../environments/environment";
 
 @Component({
   selector: "app-root",
@@ -6,4 +8,16 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "tetris";
+  constructor(private electronService: ElectronService) {
+    console.log("AppConfig", AppConfig);
+
+    if (electronService.isElectron) {
+      console.log(process.env);
+      console.log("Run in electron");
+      console.log("Electron ipcRenderer", this.electronService.ipcRenderer);
+      console.log("NodeJS childProcess", this.electronService.childProcess);
+    } else {
+      console.log("Run in browser");
+    }
+  }
 }
--- projects/tetris/src/app/app.module.ts
+++ projects/tetris/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
 import { GameEngineLibModule } from "@game-engine-lib";
 
 import { AppComponent } from "./app.component";
+import { CoreModule } from "./core/core.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, GameEngineLibModule],
+  imports: [BrowserModule, GameEngineLibModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
