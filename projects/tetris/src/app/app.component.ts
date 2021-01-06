366ab25e4d5859b7b684bbebc4899e91919dbe86
Richard Sithole <richardsit@gmail.com>
2021-01-06 11:20:24 +0100

    Clean up

--- projects/tetris/src/app/app.component.ts
+++ projects/tetris/src/app/app.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import {
-  CapacitorStorageService,
-  ElectronService,
-  PlatformService,
-} from "@shared-lib";
-
-import * as AppConfig from "../environments/environment";
+import { ElectronService, PlatformService } from "@shared-lib";
 
 @Component({
   selector: "app-root",
@@ -13,13 +7,11 @@ import * as AppConfig from "../environments/environment";
 })
 export class AppComponent implements OnInit {
   title = "tetris";
+
   constructor(
     private platform: PlatformService,
-    private electronService: ElectronService,
-    private capStorageService: CapacitorStorageService
-  ) {
-    this.localStorageSet("highscore", 2);
-  }
+    private electronService: ElectronService
+  ) {}
 
   async ngOnInit() {
     if (this.platform.isElectron) {
@@ -27,32 +19,10 @@ export class AppComponent implements OnInit {
       // console.log(process.env);
       console.log("Electron ipcRenderer", this.electronService.ipcRenderer);
       console.log("NodeJS childProcess", this.electronService.childProcess);
-      const hs = await this.localStorageGet("highscore");
-      console.info("hs is: ", hs);
     } else if (this.platform.isNative) {
       console.log("Run in native");
-      const hs = await this.localStorageGet("highscore");
-      console.info("hs is: ", hs);
     } else {
       console.log("Run in browser");
-      const hs = await this.localStorageGet("highscore");
-      console.info("hs is: ", hs);
     }
-  }
-
-  async localStorageGet(key: string): Promise<any> {
-    if (this.platform.isElectron) {
-      (window as Window).localStorage.getItem(key);
-    }
-
-    return await this.capStorageService.get(key);
-  }
-
-  localStorageSet(key: string, value: any): void {
-    if (this.platform.isElectron) {
-      (window as Window).localStorage.setItem(key, value);
-    }
-
-    this.capStorageService.set(key, value);
   }
 }
